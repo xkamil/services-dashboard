4042cb921d4313f692e11b2458019714eba91dc8
Kamil Wrobel <kamil.wrobel@symmetrical.ai>
2026-06-10 13:00:38 +0200

    chore: Add api token guard

--- src/app/(dashboard)/environments/layout.tsx
+++ src/app/(dashboard)/environments/layout.tsx
@@ -1,6 +1,8 @@
 import { Container } from "@chakra-ui/react";
 import { type ReactNode } from "react";
 
+import { SecretGuard } from "~/app/_components/secret-guard";
+
 import { EnvironmentsNav } from "./_components/environments-nav";
 
 export default function EnvironmentsLayout({
@@ -10,7 +12,7 @@ export default function EnvironmentsLayout({
     <>
       <EnvironmentsNav />
       <Container maxW="6xl" py={8}>
-        {children}
+        <SecretGuard secret="JENKINS_API_TOKEN">{children}</SecretGuard>
       </Container>
     </>
   );
